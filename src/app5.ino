a31fad934b08dd5f96a58d94f5523ed10331d256
Alexandre Bergeron <bera2920@usherbrooke.ca>
2021-07-11 18:12:01 -0400

    envoie du uuid sur le cloud
    Co-authored-by: mattp000 <mattp000@users.noreply.github.com>

--- src/app5.ino
+++ src/app5.ino
@@ -5,8 +5,39 @@
  * Date:
  */
 
+String UUID;
 int compteur = 0;
 int sortie_A0 = 0;
+
+void scanBluethoot(const BleScanResult *scanResult, void *context)
+{
+  Serial.println("Scan");
+  int taille = scanResult->advertisingData().length();
+  Serial.printlnf("taille = %i", taille);
+
+  if(taille == 27)
+  {
+    UUID = "";
+    unsigned char* array = new unsigned char[taille+1];
+    memset(array, 0, taille+1);
+    scanResult->advertisingData().get(array, taille);
+
+    // 0X02 et 0x15 == iBeacon Advertiser
+    if(array[4] == 0x02 && array[5] == 0x15)
+    {
+      for(int i = 6; i<22; i++)
+      {
+        UUID += String(array[i], HEX);
+        Serial.printf("%02x", array[i]);
+      }
+      bool success = Particle.publish("uuid", NULL, WITH_ACK);
+      if (!success) {
+        
+        Serial.println("erreur de transmission du uuid");
+      }
+    }
+  }
+}
 
 void callback_lecture_A0(const char *event, const char *data)
 {
@@ -20,11 +51,17 @@ void setup() {
   delay(1000);
   Serial.println("Start");
   WiFi.on();
+
   // C'est marqué de les mettres au début pour le mode automatic
   Particle.variable("compteur", compteur);
   // Particle.variable("output/A0", sortie_A0);
   Particle.subscribe("output/AO", callback_lecture_A0);
 
+  Particle.variable("uuid", UUID);
+
+  // Appel du callback quand un scan
+  BLE.on();
+  BLE.begin();
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -32,6 +69,8 @@ void loop() {
   // Connection au cloud de particle (C'est sensé se faire tout seul)
   //On est 3!!
 
+  BLE.scan(scanBluethoot, NULL);
+  
   Serial.println("Publish le compteur");
 
   bool success = Particle.publish("compteur", NULL, WITH_ACK);
@@ -39,6 +78,5 @@ void loop() {
     Serial.println("You Failed");
   }
   compteur++;
-  delay(1000);
   
 }
